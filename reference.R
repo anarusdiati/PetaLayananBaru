ee24808e000699aef01677cef50a8f8d64fe0ccf
Ana Rusdiati <anausername@gmail.com>
2020-03-18 17:40:01 +0700

    Add files via upload

---
+++ reference.R
@@ -0,0 +1,261 @@
+# create references
+
+# Pelayanan Medik Umum
+pelayanan01 <- list("Pelayanan Medik Umum"=c("Pelayanan medik dasar / umum",	
+                 "Pelayanan medik gigi mulut", 	
+                 "Pelayanan KIA/KB"))
+
+# Pelayanan Gawat Darurat 
+pelayanan02 <- list("Pelayanan Gawat Darurat"=c("Pelayanan Gawat Darurat Umum 24 jam & 7 hari seminggu"))
+
+# Pelayanan Medik Spesialis Dasar
+pelayanan03 <- list("Pelayanan Medik Spesialis Dasar"=c("Penyakit dalam",
+                 "Kesehatan anak",
+                 "Bedah",
+                 "Obstetri dan ginekologi"))
+
+# Pelayanan Spesialis Penunjang Medik
+pelayanan04 <- list("Pelayanan Spesialis Penunjang Medik"=c("Anestesi",
+                 "Radiologi",
+                 "Patologi Klinik",
+                 "Patologi Anatomi",
+                 "Rehabilitasi Medik"))
+
+# Pelayanan Medik Spesialis Lain 
+pelayanan05 <- list("Pelayanan Medik Spesialis Lain"=c("Mata",	
+                 "Paru",	
+                 "Kulit dan Kelamin",	
+                 "Kedokteran Jiwa / Psikiatri/  Psikogeriatri/ NAPZA",	
+                 "Orthopedi",	
+                 "Telinga Hidung Tenggorok Kepala Leher",	
+                 "Saraf",	
+                 "Jantung dan Pembuluh Darah",	
+                 "Kedokteran Forensik",	
+                 "Urologi",	
+                 "Bedah Plastik Rekonstruksi dan Estetika",	
+                 "Bedah Saraf",	
+                 "CT Scan	Laboratorium mikrobiologi",	
+                 "Metadon",	
+                 "Bedah thorax kardiak dan vaskuler",	
+                 "Bedah Anak",	
+                 "Radioterapi/Onkologi Radiasi",	
+                 "Radiologi Kedokteran Gigi",	
+                 "Parasitologi Klinik",	
+                 "Farmakologi klinik",	
+                 "Kedokteran Nuklir",	
+                 "Emergensi"))
+
+# Pelayanan Medik Spesialis Lain berupa Pelayanan Spesialistik Mata di RS Mata
+pelayanan06 <- list("Pelayanan Medik Spesialis Lain berupa Pelayanan Spesialistik Mata di RS Mata"=c("Mata Infeksi dan Imunologi", 	
+                 "Mata Glaukoma",	
+                 "Bedah Katarak",	
+                 "Bedah vitreo retina lanjut",	
+                 "Mata Komunitas",	
+                 "Mata Pediatrik",	
+                 "Onkologi Rekontruksi Mata",	
+                 "Refraksi dan Lensa Kontak",	
+                 "Kornea Lensa dan Bedah refraktif",	
+                 "Oftamologi Komunitas",	
+                 "Mata Strabismus",	
+                 "Saraf Mata"))
+
+# Pelayanan Medik Spesialis Lain berupa Pelayanan Spesialistik Jantung dan Pembuluh Darah di RS Jantung
+pelayanan07 <- list("Pelayanan Medik Spesialis Lain berupa Pelayanan Spesialistik Jantung dan Pembuluh Darah di RS Jantung"=c("Jantung konservatif",
+                 "Jantung Intervensi",
+                 "Jantung koroner	Gagal Jantung Kronik",
+                 "Jantung Hipertensi",
+                 "Jantung Aritmia dan reprogram alat pacu jantung",
+                 "Kardiometabolik",
+                 "Jantung Vaskuler",
+                 "Jantung Valvular",
+                 "Pasca intervensi non bedah (Jantung)",
+                 "Pasca Operasi CABG (Jantung)",
+                 "Pasca Operasi katup (Jantung)",
+                 "Pasca operasi Pediatrik (Jantung)",
+                 "Jantung perikard",
+                 "Penyakit Jantung Pada Kehamilan",
+                 "Bedah jantung anak dan PJB",
+                 "Bedah jantung dewasa",
+                 "Prevensi Rehabilitasi Jantung"))
+
+# Pelayanan Medik Spesialis Lain berupa Pelayanan Spesialistik Jiwa dan KO
+pelayanan08 <- list("Pelayanan Medik Spesialis Lain berupa Pelayanan Spesialistik Jiwa dan KO"=c("Pelayanan kesehatan tumbuh kembang anak dan remaja",	
+                 "Pelayanan kesehatan jiwa dewasa",	
+                 "Pelayanan kesehatan jiwa lansia",	
+                 "Gangguan mental organik",	
+                 "Pelayanan psikologi dan psikometri",	
+                 "Pelayanan kesehatan jiwa masyarakat",	
+                 "Pelayanan konseling dan psikoterapi",	
+                 "Rehab Mental",	
+                 "Forensik Adiksi"))
+
+# Pelayanan Medik Spesialis Lain berupa Pelayanan Spesialistik Paru di RS Paru
+pelayanan09 <- list("Pelayanan Medik Spesialis Lain berupa Pelayanan Spesialistik Paru di RS Paru"=c("Infeksi Paru",	
+                 "Paru Infeksi",	
+                 "Paru Asma dan PPOK", 	
+                 "Paru Onkologi",	
+                 "Paru Fisiologi",	
+                 "Penyakit paru akibat kerja",
+                 "Paru Imunologi",	
+                 "Paru Intervensi"))
+
+# Pelayanan Medik Spesialis Lain berupa Pelayanan Spesialistik Orthopedi di RS Orthopedi
+pelayanan10 <- list("Pelayanan Medik Spesialis Lain berupa Pelayanan Spesialistik Orthopedi di RS Orthopedi"=c("Lower Extremity Surgery (Hip and Knee)",	
+                 "Spine Surgery",	
+                 "Hand & Micro Surgery",	
+                 "Paediatric Surgery (Ortophaedi)",	
+                 "Knee and Ankle",	
+                 "Sport, Shoulder, and Elbow",	
+                 "Tumor Muskuloskeletal"))
+
+# Pelayanan Medik Spesialis Gigi dan Mulut 
+pelayanan11 <- list("Pelayanan Medik Spesialis Gigi dan Mulut"=c("Bedah Mulut",	
+                 "Konservasi / endodonsi",
+                 "Orthodonti",	
+                 "Periodonti",	
+                 "Prosthodonti",	
+                 "Pedodonsi", 	
+                 "Penyakit Mulut"))
+
+# Pelayanan Medik Subspesialis Bedah 
+pelayanan12 <- list("Pelayanan Medik Subspesialis Bedah"=c("Bedah vaskular",
+                 "Bedah digestif",
+                 "Bedah onkologi"))
+
+# Pelayanan Medik Subspesialistik Anak 
+pelayanan13 <- list("Pelayanan Medik Subspesialistik Anak"=c("Perinatologi",	
+                 "Neurologi",	
+                 "Hemato-Onkologi",	
+                 "Nefrologi", 	
+                 "Respirologi", 	
+                 "Alergi Imunologi",
+                 "Endokrin",	
+                 "Nutrisi dan Metabolic",	
+                 "Kardiologi Anak",	
+                 "Infeksi dan Penyakit Tropis",	
+                 "Tumbuh Kembang dan Pediatri Sosial",	
+                 "Pencitraan Anak"))
+
+# Pelayanan Medik Subspesialistik Kebidanan dan Kandungan 
+pelayanan14 <- list("Pelayanan Medik Subspesialistik Kebidanan dan Kandungan"=c("Fetomaternal (Perinatologi kebidanan)",	
+                 "Onkologi Ginekologi",	
+                 "Fertilitas dan Endokrinologi Reproduksi (Kesehatan Reproduksi)",	
+                 "Obgyn sosial", 	
+                 "Uro-ginekologi Rekonstruksi"))
+
+# Pelayanan Medik Subspesialistik Mata 
+pelayanan15 <- list("Pelayanan Medik Subspesialistik Mata"=c("Vitreo Retina",
+                 "Strabismus",	
+                 "Pediatrik Oftamologi"))
+
+# Pelayanan Medik Subspesialis THT
+pelayanan16 <- list("Pelayanan Medik Subspesialis THT"=c("Bedah THT KL dan Rekonstruksi",	
+                 "Alergi Imunologi",	
+                 "Bera (Brain Evoke Response Audimetri)",	
+                 "E.N.G (Electric Nistamografi)",	
+                 "Audiovestibuler",	
+                 "Hearing Aid Center"))
+
+# Pelayanan Medik Subspesialis Saraf 
+pelayanan17 <- list("Pelayanan Medik Subspesialis Saraf"=c("Stroke dan Cerebro Vaskuler", 	
+                 "Neuro fisiologi",	
+                 "Neuro emergency / intensive",	
+                 "Neuro restorasi/fungsi luhur",	
+                 "Neuro optalmologi / otologi",	
+                 "Neuro onkologi",	
+                 "Epilepsi"))
+
+# Pelayanan Lainnya 
+pelayanan18 <- list("Pelayanan Lainnya"=c("Elektromedik diagnostik (EKG/EEG/EEG Brain Mapping)",	
+                 "Pelayanan Intensif PICU"))
+
+# Pelayanan Kefarmasian 
+pelayanan19 <- list("Pelayanan Kefarmasian"=c("Pelayanan farmasi"))
+
+# Pelayanan Penunjang Klinik 
+pelayanan20 <- list("Pelayanan Penunjang Klinik"=c("Bank darah",	
+                 "Sterilisasi / CSSD",	
+                 "Rekam medik"))
+
+# Pelayanan Penunjang Non Klinik 
+pelayanan21 <- list("Pelayanan Penunjang Non Klinik"=c("Pemeliharaan Sarana, Prasarana dan fasilitas",	
+                 "Pengelolaan limbah / kesehatan lingkungan",	
+                 "Sistem informasi dan komunikasi / SIRS / IT",	
+                 "Pemulasaran jenazah"))
+
+# Pelayanan Rumah Sakit KO 
+pelayanan22 <- list("Pelayanan Rumah Sakit KO"=c("Isolasi Pasien Gaduh Gelisah"))
+
+# Pelayanan Mata di RS Mata 
+pelayanan23 <- list("Pelayanan Mata di RS Mata"=c("Bank Mata"))
+
+# Pelayanan Fasilitas Penunjang di RS Stroke 
+pelayanan24 <- list("Pelayanan Fasilitas Penunjang di RS Stroke "=c("Akupuntur",	
+                 "Kedokteran Olah raga",	
+                 "Neurorehabilitasi"))
+
+# Pelayanan Medik Subspesialis Penyakit Dalam
+pelayanan25 <- list("Pelayanan Medik Subspesialis Penyakit Dalam"=c("Dialisis/ CAPD",	
+                 "Gastroenterologi Hepatologi",	
+                 "Alergi Imunologi",	
+                 "Geriatri",	
+                 "Ginjal dan Hipertensi",
+                 "Hemato-Onkologi",	
+                 "Kardiovaskuler",	
+                 "Metabolik Endokrin",	
+                 "Psikosomatik",	
+                 "Pulmonologi",	
+                 "Reumatologi",	
+                 "Paliatif"))
+
+# Jumlah RS
+pelayanan26 <- list("Jumlah RS"=c("Jumlah RS"))
+
+# Create list of reference
+pelayananRefs <- c(pelayanan01,pelayanan02,pelayanan03,pelayanan04,pelayanan05,
+                   pelayanan06,pelayanan07,pelayanan08,pelayanan09,pelayanan10,
+                   pelayanan11,pelayanan12,pelayanan13,pelayanan14,pelayanan15,
+                   pelayanan16,pelayanan17,pelayanan18,pelayanan19,pelayanan20,
+                   pelayanan21,pelayanan22,pelayanan23,pelayanan24,pelayanan25,
+                   pelayanan26)
+
+# Save to RDS
+saveRDS(pelayananRefs,"data/pelayananRefs.rds")
+
+provinsi <- list("Provinsi"=c("Semua","Aceh",
+                              "Sumatera Utara",
+                              "Sumatera Barat",
+                              "Riau",
+                              "Jambi",
+                              "Sumatera Selatan",
+                              "Bengkulu",
+                              "Lampung",
+                              "Kepulauan Bangka Belitung",
+                              "Kepulauan Riau",
+                              "Daerah Khusus Ibukota Jakarta",
+                              "Jawa Barat",
+                              "Jawa Tengah",
+                              "Daerah Istimewa Yogyakarta",
+                              "Jawa Timur",
+                              "Banten",
+                              "Bali",
+                              "Nusa Tenggara Barat",
+                              "Nusa Tenggara Timur",
+                              "Kalimantan Barat",
+                              "Kalimantan Tengah",
+                              "Kalimantan Selatan",
+                              "Kalimantan Timur",
+                              "Kalimantan Utara",
+                              "Sulawesi Utara",
+                              "Sulawesi Tengah",
+                              "Sulawesi Selatan",
+                              "Sulawesi Tenggara",
+                              "Gorontalo",
+                              "Sulawesi Barat",
+                              "Maluku",
+                              "Maluku Utara",
+                              "Papua Barat",
+                              "Papua"))
+
+saveRDS(provinsi,"data/provinsiRefs.rds")
+
